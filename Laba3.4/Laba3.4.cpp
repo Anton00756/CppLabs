4bec7bb70f9600fd2082c64c9e6ad2f5b34b0345
Антон Комиссаров <anton.komissarov.02@mail.ru>
2021-12-04 17:58:29 +0300

    Добавьте файлы проекта.

---
+++ Laba3.4/Laba3.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	cout << "\tЛабораторная работа №3.4\n\tМ8О-211Б-20: Комиссаров Антон\n\n";
+
+	
+	system("pause>nul");
+}
